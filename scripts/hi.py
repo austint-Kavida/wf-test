6f4a6e1369ee0576ef661cdb3c5de33bf1f7e490
austint-Kavida <163855510+austint-Kavida@users.noreply.github.com>
2024-06-18 03:59:42 -0700

    linter 2

--- scripts/hi.py
+++ scripts/hi.py
@@ -1,4 +1,10 @@
-# a script that takes in a number and prints is square 
+# This script takes in a number and prints its square
+
+# Prompt the user to enter a number
 num = float(input("Enter a number: "))
+
+# Calculate the square of the number
 square = num ** 2
+
+# Print the result
 print("The square of", num, "is", square)
